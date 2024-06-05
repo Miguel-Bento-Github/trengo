0a5ae5a7c4195186914cc2424554be161f97bd83
Miguel Bento <bento-miguel@outlook.com>
2024-06-06 00:00:18 +0200

    add persisted state to pinia

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import "@/styles/boot.css";
 import { router } from "@/plugins/router";
 import { FontAwesomeIcon } from "@/plugins/fontawesome";
 import { createPinia } from "pinia";
+import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 import { createApp } from "vue";
 
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedState);
+
 createApp(App)
-  .use(createPinia())
+  .use(pinia)
   .use(router)
   .component("FontAwesomeIcon", FontAwesomeIcon)
   .mount("#app");
