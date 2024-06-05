f8e701437ebc69489d97db41ea740f3fff34ba3a
Miguel Bento <bento-miguel@outlook.com>
2024-06-06 00:02:16 +0200

    add favorite list and controls

---
+++ src/components/molecules/FavoriteList.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { animations } from "@formkit/drag-and-drop";
+import { useDragAndDrop } from "@formkit/drag-and-drop/vue";
+import { useChannelStore } from "@/stores/useChannelStore";
+import { storeToRefs } from "pinia";
+import { watch } from "vue";
+import ChannelItem from "@/components/ChannelItem.vue";
+
+const channelStore = useChannelStore();
+const { favorites: favoritesRef } = storeToRefs(channelStore);
+const [favoriteRef, favoriteList] = useDragAndDrop(favoritesRef.value, {
+  plugins: [animations()],
+  dragHandle: ".drag-handle",
+});
+
+// Updates store list on hook list change
+channelStore.$subscribe(
+  () => {
+    favoriteList.value = favoritesRef.value;
+  },
+  { detached: true }
+);
+
+// Keeps order on list change
+watch(favoriteList, (newList) => {
+  channelStore.favorites = newList;
+});
+</script>
+
+<template>
+  <ul ref="favoriteRef" class="max-h-96 overflow-y-auto rounded-md">
+    <TransitionGroup name="content">
+      <li v-for="favorite in favoriteList" :key="favorite" class="min-h-5">
+        <ChannelItem
+          :item="channelStore.getChannelById(favorite)"
+          @remove="channelStore.removeFavorite(favorite)"
+        />
+      </li>
+    </TransitionGroup>
+  </ul>
+</template>
---
+++ src/components/molecules/FavoriteListControls.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useChannelStore } from "@/stores/useChannelStore";
+import PopupButton from "@/components/atoms/PopupButton.vue";
+
+const channelStore = useChannelStore();
+</script>
+
+<template>
+  <Transition name="content" mode="out-in">
+    <div
+      v-if="channelStore.isStateChanged"
+      class="mt-2 flex justify-end gap-x-2"
+    >
+      <PopupButton @click="channelStore.reset()"> Cancel </PopupButton>
+      <PopupButton :isDark="true" @click="channelStore.save()">
+        Apply
+      </PopupButton>
+    </div>
+  </Transition>
+</template>
