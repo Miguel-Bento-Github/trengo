92b90f68e7139f6e75445bfa37ea69f5ae65341a
Miguel Bento <bento-miguel@outlook.com>
2024-06-05 23:59:41 +0200

    add cross cta

---
+++ src/components/atoms/CrossButton.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import MainButton from "./MainButton.vue";
+
+const emit = defineEmits<{
+  (e: "click"): void;
+}>();
+</script>
+
+<template>
+  <span class="relative flex justify-end">
+    <MainButton
+      @click="emit('click')"
+      class="cross text-xl fixed -translate-y-4 translate-x-6 hover:text-red-900"
+      aria-label="close popup"
+    >
+      <FontAwesomeIcon icon="times-circle" />
+    </MainButton>
+  </span>
+</template>
+
+<style scoped>
+/* increases hit point ratio for better accessibility */
+.cross::before {
+  position: absolute;
+  content: "";
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+  cursor: pointer;
+}
+</style>
