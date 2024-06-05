aa2a887ddb9ea4a99b1adf1141c93f7b06f6443b
Miguel Bento <bento-miguel@outlook.com>
2024-06-06 00:01:32 +0200

    add form fieldset component

---
+++ src/components/molecules/FormFieldset.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import InputPopup from "../InputPopup.vue";
+
+const emit = defineEmits<{
+  (e: "enter", value: string): void;
+}>();
+
+defineProps<{ placeholder: string; description: string; name: string }>();
+</script>
+
+<template>
+  <form @submit.prevent="">
+    <fieldset>
+      <InputPopup
+        @keydown.enter="emit('enter', $event.target.value)"
+        :name="name"
+        :placeholder="placeholder"
+        :description="description"
+      />
+    </fieldset>
+  </form>
+</template>
