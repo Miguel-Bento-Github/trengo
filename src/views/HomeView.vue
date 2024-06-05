d0a83f36a39b2320adcad1baa576a003217db601
Miguel Bento <bento-miguel@outlook.com>
2024-06-06 00:02:28 +0200

    connect everything at the homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,47 +1,72 @@
 <script setup lang="ts">
-const isLoading = false;
-const isError = false;
-const data = {
-  name: "Team@trengo.com",
+import { ref } from "vue";
+import { useChannelStore } from "@/stores/useChannelStore";
+import { getRandomItemInArray } from "@/util/get-random-item-in-array";
+import { iconList } from "@/const/icon-list";
+import { v4 as generateId } from "uuid";
+import FavoriteList from "@/components/molecules/FavoriteList.vue";
+import FavoriteListControls from "@/components/molecules/FavoriteListControls.vue";
+import CrossButton from "@/components/atoms/CrossButton.vue";
+import ListItem from "@/components/atoms/ListItem.vue";
+import FormFieldset from "@/components/molecules/FormFieldset.vue";
+
+const channelStore = useChannelStore();
+
+const isDialogOpen = ref(false);
+
+const addFavorite = (id: string) => {
+  const isAdded = channelStore.addFavorite(id);
+  if (!isAdded) return;
+  isDialogOpen.value = false;
+};
+
+const createEntry = (name: string) => {
+  const icon = getRandomItemInArray(iconList);
+  const newEntry = {
+    id: generateId(),
+    name,
+    icon,
+  };
+  channelStore.createEntry(newEntry);
 };
 </script>
 
 <template>
-  <h1>Welcome to Trengo</h1>
-  <p :aria-busy="isLoading">
-    <TransitionGroup name="content">
-      <span key="loading" v-if="isLoading">Loading...</span>
-      <div key="error" v-if="isError">Something went wrong</div>
-      <span key="not-loading" data-cy="home-content" v-else>
-        <div>
-          <FontAwesomeIcon :icon="['fas', 'grip-vertical']" />
-          <FontAwesomeIcon :icon="['fab', 'whatsapp']" />
-          {{ data.name }}
-          <button type="button">Remove</button>
+  <h1 class="text-xl font-semibold mx-auto mb-4 w-10/12 max-w-4xl">Channels</h1>
+  <main class="mx-auto w-10/12 max-w-4xl shadow-lg p-4 rounded-lg">
+    <div>
+      <FormFieldset
+        placeholder="Add channel"
+        description="Please provide a channel name"
+        name="popup"
+        @enter="createEntry"
+        @click="isDialogOpen = true"
+      >
+      </FormFieldset>
+      <dialog
+        aria-modal="true"
+        ref="dialog"
+        id="popup"
+        class="relative w-full"
+        :open="isDialogOpen"
+      >
+        <div
+          class="absolute bg-white overflow-y-auto max-h-80 p-4 rounded-md shadow-md w-full min-w-80"
+        >
+          <CrossButton @click="isDialogOpen = false" />
+          <ListItem
+            @add="addFavorite(channel.id)"
+            v-for="channel in channelStore.channels"
+            :key="channel.id"
+          >
+            {{ channel.name }}
+          </ListItem>
         </div>
-      </span>
-    </TransitionGroup>
-  </p>
+      </dialog>
+    </div>
+    <div>
+      <FavoriteList />
+      <FavoriteListControls />
+    </div>
+  </main>
 </template>
-
-<style lang="scss" scoped>
-.content-move,
-.content-enter-active {
-  transition: all 0.5s ease;
-}
-
-.content-leave-active {
-  transition-duration: 0.25s;
-}
-
-.content-enter-from,
-.content-leave-to {
-  opacity: 0;
-  transform: translateX(30px);
-}
-
-.content-leave-active,
-.content-enter-active {
-  position: absolute;
-}
-</style>
