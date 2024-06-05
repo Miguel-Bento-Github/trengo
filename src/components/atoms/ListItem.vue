6227f617bc8ac5ab81e006ad3bf9c20211622a99
Miguel Bento <bento-miguel@outlook.com>
2024-06-06 00:01:56 +0200

    add list item

---
+++ src/components/atoms/ListItem.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import ListButton from "@/components/atoms/ListButton.vue";
+
+const emit = defineEmits<{
+  (e: "add"): void;
+}>();
+</script>
+
+<template>
+  <div class="flex justify-between items-center pe-4">
+    <slot />
+    <ListButton type="button" @click="emit('add')"> Add </ListButton>
+  </div>
+</template>
