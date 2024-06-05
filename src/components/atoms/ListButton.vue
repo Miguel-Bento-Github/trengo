4e2171d621a82fc4f6b423c6657acffdf957417a
Miguel Bento <bento-miguel@outlook.com>
2024-06-06 00:01:10 +0200

    add list component

---
+++ src/components/atoms/ListButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import MainButton from "./MainButton.vue";
+</script>
+
+<template>
+  <MainButton class="hover:bg-gray-100 hover:text-gray-800 text-gray-600">
+    <slot />
+  </MainButton>
+</template>
