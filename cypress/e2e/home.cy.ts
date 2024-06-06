defb020764cbf5931f8e0981408d3fea9eaa9af6
Miguel Bento <document.body.title@gmail.com>
2024-06-06 12:11:56 +0200

    add home e2e test example

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,18 +1,15 @@
-describe("home page", () => {
-  it("should fetch data", () => {
-    cy.intercept("GET", "https://dummyjson.com/http/200/Hello%20World", {
-      statusCode: 200,
-      fixture: "../fixtures/data.json",
-    }).as("data");
+describe("Test headings", () => {
+  it("should have a h1 at home", () => {
+    // In a real world scenario data would be fetched and test mocked here
+    // cy.intercept("GET", "https://trengo.com/http/200/channels", {
+    //   statusCode: 200,
+    //   fixture: "../fixtures/channels.json",
+    // }).as("data");
 
-    cy.visit("/home")
-      .wait("@data")
+    cy.visit("/")
       .location()
       .should((location) => {
-        expect(location.pathname).to.equal("/home");
-      })
-      .dataCy("home-content")
-      .should("exist")
-      .should("contain.text", "Hello World");
+        expect(location.pathname).to.equal("/");
+      });
   });
 });
