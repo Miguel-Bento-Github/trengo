37256f49047921baa5036259fcc4682123ca4cbe
Miguel Bento <bento-miguel@outlook.com>
2024-06-06 12:33:20 +0200

    update fontawesome to be able to build to production

--- src/plugins/fontawesome.ts
+++ src/plugins/fontawesome.ts
@@ -1,5 +1,5 @@
 /* import the fontawesome core */
-import { library } from "@fortawesome/fontawesome-svg-core";
+import { library, IconPack } from "@fortawesome/fontawesome-svg-core";
 
 /* import font awesome icon component */
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
@@ -10,6 +10,6 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 
 /* add icons to the library */
-library.add(fas, fab, far);
+library.add(fas as IconPack, fab as IconPack, far as IconPack);
 
 export { FontAwesomeIcon };
